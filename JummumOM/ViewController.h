50bfc0a5955e648193a48ecae79ebe989aa0a0fc
Thidaporn Kijkamjai <jinglejill@me.com>
2018-08-04 15:06:09 +0700

    Initial Commit

---
+++ JummumOM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JummumOM
+//
+//  Created by Thidaporn Kijkamjai on 4/8/2561 BE.
+//  Copyright © 2561 Jummum Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JummumOM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JummumOM
+//
+//  Created by Thidaporn Kijkamjai on 4/8/2561 BE.
+//  Copyright © 2561 Jummum Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
